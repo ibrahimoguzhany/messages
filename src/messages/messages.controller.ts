8dc8cef74c089930df7cbcb75e7c9c40b204ed13
oguzhan <ibrahimoguzhany@gmail.com>
2021-11-21 20:40:09 +0300

    plainToClass error resolved

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,19 +1,32 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages-service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
   @Get()
   listMessages() {
-    console.log('asdas');
+    return this.messagesService.findAll();
   }
 
   @Post()
-  createMessage(@Body() body: any) {
-    console.log(body);
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessage(@Param() id: string) {
-    console.log(id);
+  async getMessage(@Param() id: string) {
+    const message = await this.messagesService.findOne(id);
   }
 }
